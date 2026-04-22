6e73468c203403cc69523688f3cba2632cb2ffe6
Iván Salazar <ivangio.salazar@gmail.com>
2026-04-22 04:37:54 -0600

    chore: bump version to 0.4.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ package version
 
 var (
 	// Version is the semantic version of the service.
-	Version = "0.4.0"
+	Version = "0.4.1"
 	// Commit is the short git SHA baked in at build time.
 	Commit = "dev"
 )
